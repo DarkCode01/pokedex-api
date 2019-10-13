555c5b782e852daf8772d3ed7aeaa1fcb00ddafb
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 19:46:02 -0400

    feat: add gender services

---
+++ src/app/components/gender/gender.service.ts
@@ -0,0 +1,25 @@
+import { Gender } from './gender.providers'
+
+export class GenderService {
+  _GenderMapper: any
+  _GenderRepository: any
+
+  constructor({
+    GenderMapper,
+    GenderRepository
+  }: any) {
+    this._GenderMapper = GenderMapper
+    this._GenderRepository = GenderRepository
+  }
+
+  public getOrCreateGender = async (genderName: string) : Promise<Gender> => {
+    let gender = await this._GenderRepository.getGenderByName(genderName)
+
+    if (!gender) {
+      const newGender = await this._GenderMapper.mapToEntity(genderName)
+      gender = await this._GenderRepository.saveGender(newGender)
+    }
+
+    return gender
+  }
+}
