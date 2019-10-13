f83b68d57f55a5c43423f8e7c2efef740d43753c
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 19:46:15 -0400

    feat: add gender providers file

---
+++ src/app/components/gender/gender.providers.ts
@@ -0,0 +1,5 @@
+export { Gender } from './domain/gender.entity'
+export { GenderController } from './gender.controller'
+export { GenderService } from './gender.service'
+export { GenderRepository } from './gender.repository'
+export { GenderMapper } from './domain/gender.mapper'
