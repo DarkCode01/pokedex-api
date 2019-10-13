6de12f48e745d3cdd9c5e11f26a94508483709de
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 15:17:25 -0400

    wip: remove services folder and add some test methods

--- src/app/components/user/services/create.ts
+++
@@ -1,5 +0,0 @@
-export class Create {
-  public static async build() {
-
-  }
-}
---
+++ src/app/components/user/user.service.ts
@@ -0,0 +1,43 @@
+import { UserDTO } from './user.providers'
+
+interface IProps {
+  UserMapper: any
+  UserRepository: any,
+  ErrorHandler: any,
+  codes: ApiCodes
+}
+
+export class UserService {
+  _UserMapper: any
+  _UserRepository: any
+  _ErrorHandler: any
+  _codes: ApiCodes
+
+  constructor({
+    UserMapper,
+    UserRepository,
+    ErrorHandler,
+    codes
+  }: IProps) {
+    this._UserMapper = UserMapper
+    this._UserRepository = UserRepository
+    this._ErrorHandler = ErrorHandler
+    this._codes = codes
+  }
+
+  public create = async (userPayload: any) : Promise<UserDTO> => {
+    const user = await this._UserMapper.mapToEntity(userPayload)
+
+    const isRegistered = await this._UserRepository.getUserByEmail(user.email)
+    const usernameExists = await this._UserRepository.getUserByUsername(user.username)
+
+    if (isRegistered)
+      throw this._ErrorHandler.build({
+        status: this._codes.UNAUTHORIZED,
+        msg: 'Candidate does not have a registered token'
+      })
+
+    const savedUser = await this._UserRepository.savedUser(user)
+    return this._UserMapper.mapToDTO(savedUser)
+  }
+}
