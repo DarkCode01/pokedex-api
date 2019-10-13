3967e3203c5e5efb6db47273f16b58ad551e7dc9
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 19:45:22 -0400

    feat: add gender mapper

---
+++ src/app/components/gender/domain/gender.mapper.ts
@@ -0,0 +1,14 @@
+import { Gender } from '../gender.providers'
+
+export class GenderMapper {
+  _GenderRepository: any
+
+  constructor({
+    GenderRepository
+  }: any){
+    this._GenderRepository = GenderRepository
+  }
+
+  public mapToEntity = async (name: string): Promise<Gender> =>
+    await this._GenderRepository.create({ name })
+}
