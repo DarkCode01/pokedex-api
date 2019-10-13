cbde7082274a8ae93f4189614f7f42d88c8b3742
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 16:42:29 -0400

    feat(userDTO): add decorator

--- src/app/components/user/domain/user.dto.ts
+++ src/app/components/user/domain/user.dto.ts
@@ -3,12 +3,28 @@ import { MapProp } from 'ts-simple-automapper'
 export class UserDTO {
   @MapProp()
   name: string
+
+  @MapProp()
   lastname: string
+
+  @MapProp()
   email: string
+
+  @MapProp()
   username: string
+
+  @MapProp()
   picture: string
+
+  @MapProp()
   birthdate: Date
+
+  @MapProp()
   city: string
+
+  @MapProp()
   role: string
+
+  @MapProp()
   isActive: boolean
 }
