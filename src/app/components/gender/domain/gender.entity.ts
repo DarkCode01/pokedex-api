a2858ac7464cb082132c73512f3a07575db62eb3
Efra Peralta <efra23peralta@gmail.com>
2019-10-13 19:45:07 -0400

    feat: add gender entity

---
+++ src/app/components/gender/domain/gender.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { User } from '../../user/user.providers'
+
+enum enumGender {
+  male = 'male',
+  female = 'female',
+  others = 'others',
+}
+
+@Entity({ name: 'genders' })
+export class Gender {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    type: 'enum',
+    enum: enumGender
+  })
+  name: enumGender
+
+  @OneToMany(type => User, user => user.gender)
+  users: User[]
+}
