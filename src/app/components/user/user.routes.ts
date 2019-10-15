274857ac4dde769392a2f712b0f8fd1c649cd86c
Efrain Peralta <e.peralta@gbh.com.do>
2019-10-15 09:35:56 -0400

    improvement(userRoute): add routeMethod in userRoutes

--- src/app/components/user/user.routes.ts
+++ src/app/components/user/user.routes.ts
@@ -1,5 +1,4 @@
 import { Router, Response, Request } from 'express'
-import { validationResult } from 'express-validator'
 
 // Validators
 import { createValidator, authValidator, changePassValidator } from './user.providers'
@@ -9,16 +8,19 @@ export class UserRoutes {
   private codes: ApiCodes
   private ResponseHandler: any
   private UserController: any
+  private RouteMethod: any
 
   constructor({
     UserController,
     ResponseHandler,
-    codes
+    RouteMethod,
+    codes,
   }: any) {
     this.api = Router()
     this.codes = codes
     this.UserController = UserController
     this.ResponseHandler = ResponseHandler
+    this.RouteMethod = RouteMethod
   }
 
   public get routes(): Router {
@@ -38,65 +40,38 @@ export class UserRoutes {
   }
 
   public create = async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty())
-      return res
-        .status(this.codes.BAD_REQUEST)
-        .send(this.ResponseHandler.build(errors.array(), false))
-
-    try {
-      const user = await this.UserController.create(req.body)
-      if (user)
-        return res
-          .status(this.codes.CREATE)
-          .send(this.ResponseHandler.build(user, false))
-
-    } catch (err) {
-      return res
-        .status(err.statusCode || this.codes.INTERNAL_ERROR)
-        .send(this.ResponseHandler.build(err.message))
-    }
+    this.RouteMethod.build({
+      resolve: async () => {
+        const user = await this.UserController.create(req.body)
+        if (user)
+          return res
+            .status(this.codes.CREATE)
+            .send(this.ResponseHandler.build(user, false))
+      }, req, res
+    })
   }
 
-  public auth = async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty())
-      return res
-        .status(this.codes.BAD_REQUEST)
-        .send(this.ResponseHandler.build(errors.array(), false))
-
-    try {
-      const user = await this.UserController.auth(req.body)
-      if (user)
-        return res
-          .status(this.codes.OK)
-          .send(this.ResponseHandler.build(user, false))
-
-    } catch (err) {
-      return res
-        .status(err.statusCode || this.codes.INTERNAL_ERROR)
-        .send(this.ResponseHandler.build(err.message))
-    }
+  public auth = (req: Request, res: Response) => {
+    this.RouteMethod.build({
+      resolve: async () => {
+        const user = await this.UserController.auth(req.body)
+        if (user)
+          return res
+            .status(this.codes.OK)
+            .send(this.ResponseHandler.build(user, false))
+      }, req, res
+    })
   }
 
   public changePassword = async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty())
-      return res
-        .status(this.codes.BAD_REQUEST)
-        .send(this.ResponseHandler.build(errors.array(), false))
-
-    try {
-      const response = await this.UserController.changePassword(req.user, req.body)
-      if (response)
-        return res
-          .status(this.codes.OK)
-          .send(this.ResponseHandler.build(response))
-
-    } catch (err) {
-      return res
-        .status(err.statusCode || this.codes.INTERNAL_ERROR)
-        .send(this.ResponseHandler.build(err.message))
-    }
+    this.RouteMethod.build({
+      resolve: async () => {
+        const response = await this.UserController.changePassword(req.user, req.body)
+        if (response)
+          return res
+            .status(this.codes.OK)
+            .send(this.ResponseHandler.build(response))
+      }, req, res
+    })
   }
 }
