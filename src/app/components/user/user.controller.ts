9aa4edde01b8eebfa4ffcd12b63e99b762278e0c
Efra Peralta <efra23peralta@gmail.com>
2019-10-19 16:43:03 -0400

    feat(userController): add get user

--- src/app/components/user/user.controller.ts
+++ src/app/components/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { User, UserResponses } from './user.providers'
+import { UserDTO } from './user.providers'
 
 export class UserController {
   constructor(
     private UserService: any,
   ) {}
+
+  public get = async (username: string, user: UserDTO) => {
+    const _user = await this.UserService.get(username, user)
+    if (_user)
+      return _user
+  }
 }
