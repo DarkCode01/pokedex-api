9f3008645ace36f85ac36adc560d326d578172e6
Efra Peralta <efra23peralta@gmail.com>
2019-10-19 16:42:31 -0400

    feat(userService): add method to get user

--- src/app/components/user/user.service.ts
+++ src/app/components/user/user.service.ts
@@ -1,4 +1,4 @@
-import { UserDTO, UserResponses } from './user.providers'
+import { UserDTO, UserResponses, Roles } from './user.providers'
 
 export class UserService {
   constructor(
@@ -6,6 +6,23 @@ export class UserService {
     private UserRepository: any,
     private ErrorHandler: any,
     private codes: ApiCodes,
-    private JWT: any
   ) {}
+
+  public get = async (username: string, _user: UserDTO) : Promise<UserDTO> => {
+    if (_user.username === username || _user.role === Roles.owner) {
+      const user = await this.UserRepository.getUserByUsername(username)
+      if (!user)
+        throw this.ErrorHandler.build({
+          status: this.codes.BAD_REQUEST,
+          msg: UserResponses.userNotFound
+        })
+
+      return await this.UserMapper.mapToDTO(user)
+    }
+
+    throw this.ErrorHandler.build({
+      status: this.codes.UNAUTHORIZED,
+      msg: UserResponses.unauthorized
+    })
+  }
 }
