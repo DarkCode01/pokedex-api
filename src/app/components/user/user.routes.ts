d9bdfc3702fe444b039b16cb6883ce0341ec2b0f
Efra Peralta <efra23peralta@gmail.com>
2019-10-19 22:24:13 -0400

    feat(userRoutes): add method to get all users in routes file

--- src/app/components/user/user.routes.ts
+++ src/app/components/user/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request } from 'express'
 
 // Validators
-import { getValidator } from './user.providers'
+import { getValidator, Roles } from './user.providers'
 
 export class UserRoutes {
   private readonly api: Router = Router()
@@ -11,13 +11,22 @@ export class UserRoutes {
     private ResponseHandler: any,
     private RouteMethod: any,
     private codes: ApiCodes,
-    private AuthMiddleware: any
+    private AuthMiddleware: any,
+    private OwnerMiddleware: any,
   ) {}
 
   public get routes(): Router {
+    // @Desc        Get List of Users
+    // @Access      Private
+    this.api.get('/users',
+      this.AuthMiddleware.ensureAuth,
+      this.OwnerMiddleware.isOwner,
+      this.list
+    )
+
     // @Desc        Get User
     // @Access      Private
-    this.api.get('/:username',
+    this.api.get('/user/:username',
       getValidator as Array<any>,
       this.AuthMiddleware.ensureAuth,
       this.get
@@ -26,7 +35,7 @@ export class UserRoutes {
     return this.api
   }
 
-  public get = async (req: Request, res: Response) => {
+  public get = async (req: Request, res: Response) =>
     this.RouteMethod.build({
       resolve: async () => {
         const user = await this.UserController.get(req.params.username, req.user)
@@ -36,6 +45,19 @@ export class UserRoutes {
             .send(this.ResponseHandler.build(user, false))
       }, req, res
     })
-  }
 
+  public list = async (req: Request, res: Response) =>
+    this.RouteMethod.build({
+      resolve: async () => {
+        const { page, perPage } = req.query
+        const users = await this.UserController.list({
+          perPage,
+          page,
+        })
+        if (users)
+          return res
+            .status(this.codes.OK)
+            .send(this.ResponseHandler.build(users, false))
+      }, req, res
+    })
 }
