18ddc425721afe63b208b0dc6d19f8bb3d45ed88
Efra Peralta <efra23peralta@gmail.com>
2019-10-23 19:39:58 -0400

    feat(pokedexController): add disable method

--- src/application/pokedex/pokedex.controller.ts
+++ src/application/pokedex/pokedex.controller.ts
@@ -1,10 +1,25 @@
 import { PokedexDTO } from './pokedex.providers'
+import { UserDTO } from '../user/user.providers'
 
 export class PokedexController {
   constructor(
     private PokedexService: any,
   ) {}
 
-  public get = async (userId: number): Promise<PokedexDTO> =>
-    await this.PokedexService.get(userId)
+  /**
+  * @description Get a user's pokedex and if it doesn't exist create a pokedex.
+  * @param {number} userId
+  * @returns {Promise<PokedexDTO>}
+  */
+  public get = async (userId: number, userLogged: UserDTO): Promise<PokedexDTO> =>
+    await this.PokedexService.get(userId, userLogged)
+
+  /**
+  * @description Change Pokedex status
+  * @example true to false \ false to true
+  * @param {number} userId
+  * @returns {Promise<string>}
+  */
+  public toggleStatus = async (userId: number): Promise<string> =>
+    await this.PokedexService.toggleStatus(userId)
 }
