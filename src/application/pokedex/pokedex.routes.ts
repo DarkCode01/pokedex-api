656bc239e27d4ff24975e914b7d68b3daba932ea
Efra Peralta <efra23peralta@gmail.com>
2019-10-23 19:41:12 -0400

    feat(pokedexRoutes): add disable route

--- src/application/pokedex/pokedex.routes.ts
+++ src/application/pokedex/pokedex.routes.ts
@@ -26,13 +26,37 @@ export class PokedexRoutes {
       this.get
     )
 
+    /**
+    * @description Toggle pokedex status
+    * @private
+    */
+    this.api.put('/:userId/pokedex/toggle_status',
+      getValidator as Array<any>,
+      [
+        this.AuthMiddleware.ensureAuth,
+        this.OwnerMiddleware.isOwner,
+      ],
+      this.toggleStatus
+    )
+
     return this.api
   }
 
   public get: RequestHandler = (req: Request, res: Response) =>
     this.RouteMethod.build({
       resolve: async () => {
-        const pokedex = await this.PokedexController.get(req.params.userId)
+        const pokedex = await this.PokedexController.get(req.params.userId, req.user)
+        if (pokedex)
+          return res
+            .status(this.codes.OK)
+            .send(this.ResponseHandler.build(pokedex, false))
+      }, req, res
+    })
+
+  public toggleStatus: RequestHandler = (req: Request, res: Response) =>
+    this.RouteMethod.build({
+      resolve: async () => {
+        const pokedex = await this.PokedexController.toggleStatus(req.params.userId)
         if (pokedex)
           return res
             .status(this.codes.OK)
