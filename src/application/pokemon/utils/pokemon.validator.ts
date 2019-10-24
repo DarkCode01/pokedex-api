95940beed56d8a74bfb45809f6b4c5c47b24bd1d
Efra Peralta <efra23peralta@gmail.com>
2019-10-23 22:49:43 -0400

    feat(pokemonValidator): add create validators

--- src/application/pokemon/utils/pokemon.validator.ts
+++ src/application/pokemon/utils/pokemon.validator.ts
@@ -1,15 +1,36 @@
-import { param } from 'express-validator'
+import { body } from 'express-validator'
 import { PokemonResponses } from './pokemon.responses'
 
 const { validator } = PokemonResponses
 
-const getValidator = [
-  param('userId', validator.userId)
-    .exists()
-    .toInt()
-    .isInt()
+const createValidator = [
+  body('name', validator.name)
+    .trim()
+    .isLength({ min: 2 }),
+  body('description', validator.description)
+    .trim()
+    .isLength({ min: 8 }),
+  body('latitude', validator.latitude)
+    .optional()
+    .trim()
+    .isLength({ min: 3 }),
+  body('longitude', validator.longitude)
+    .optional()
+    .trim()
+    .isLength({ min: 3 }),
+  body('captured', validator.captured)
+    .optional()
+    .isLength({ min: 4 })
+    .toBoolean()
+    .isBoolean(),
+  body('height', validator.height)
+    .optional()
+    .exists(),
+  body('weight', validator.weight)
+    .optional()
+    .exists(),
 ]
 
 export {
-  getValidator,
+  createValidator,
 }
