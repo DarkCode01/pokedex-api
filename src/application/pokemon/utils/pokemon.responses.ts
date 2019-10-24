ef0bce0cf8dadccac9d1105486512374a6b415d3
Efra Peralta <efra23peralta@gmail.com>
2019-10-23 22:50:08 -0400

    feat(pokemonResponses): add validator responses

--- src/application/pokemon/utils/pokemon.responses.ts
+++ src/application/pokemon/utils/pokemon.responses.ts
@@ -1,5 +1,12 @@
 export const PokemonResponses: any = {
   validator: {
-    pokedexId: 'Please, enter a valid userId.',
+    name: 'Please, enter a name with 2 or more characters.',
+    description: 'Please, enter a valid description with 10 or more characters.',
+    latitude: 'Please, enter a valid latitude.',
+    longitude: 'Please, enter a valid longitude.',
+    captured: 'Please, enter a valid value [true or false].',
+    height: 'Please, enter a valid height.',
+    weight: 'Please, enter a valid weight.',
   },
+  pokedexDoesNotExist: 'Oops! We haven\'t found your pokedex.',
 }
